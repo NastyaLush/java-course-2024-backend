e3e31081d974501b527c01016bc2932adda1f0fa
NastyaLush <yfcn.if1203@gmail.com>
2024-02-19 14:36:09 +0300

    checkstyle fixed

--- bot/src/main/java/edu/java/bot/manager/UserMessageProcessor.java
+++ bot/src/main/java/edu/java/bot/manager/UserMessageProcessor.java
@@ -3,7 +3,6 @@ package edu.java.bot.manager;
 import com.pengrad.telegrambot.model.Update;
 import com.pengrad.telegrambot.request.SendMessage;
 import edu.java.bot.command.Command;
-import org.springframework.beans.factory.annotation.Autowired;
 
 public interface UserMessageProcessor {
 
