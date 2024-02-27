0ca7f52c283a39473400238a95e7890deddcef1c
NastyaLush <yfcn.if1203@gmail.com>
2024-02-27 16:26:58 +0300

    add exceptions

--- bot/src/main/java/edu/java/bot/controller/ControllerExceptionHandler.java
+++ bot/src/main/java/edu/java/bot/controller/ControllerExceptionHandler.java
@@ -1,5 +1,6 @@
 package edu.java.bot.controller;
 
+import com.pengrad.telegrambot.TelegramException;
 import org.apache.kafka.common.errors.ResourceNotFoundException;
 import org.springdoc.api.ErrorMessage;
 import org.springframework.http.HttpStatus;
@@ -11,9 +12,14 @@ import org.springframework.web.context.request.WebRequest;
 @RestControllerAdvice
 public class ControllerExceptionHandler {
     @ExceptionHandler(value = {ResourceNotFoundException.class})
-    @ResponseStatus(value = HttpStatus.NOT_FOUND)
+    @ResponseStatus(value = HttpStatus.NOT_FOUND, reason = "ресурса не существует")
     public ErrorMessage resourceNotFoundException(ResourceNotFoundException ex, WebRequest request) {
+        return new ErrorMessage(ex.getMessage());
+    }
 
+    @ExceptionHandler(value = {TelegramException.class})
+    @ResponseStatus(value = HttpStatus.BAD_REQUEST, reason = "ошибка при работе с телеграммом")
+    public ErrorMessage tgChatIdNotFoundException(TelegramException ex, WebRequest request) {
         return new ErrorMessage(ex.getMessage());
     }
 }
--- scrapper/src/main/java/edu/java/controller/ControllerExceptionHandler.java
+++ scrapper/src/main/java/edu/java/controller/ControllerExceptionHandler.java
@@ -1,5 +1,7 @@
 package edu.java.controller;
 
+import edu.java.exception.AlreadyExistException;
+import edu.java.exception.NotExistException;
 import org.apache.kafka.common.errors.ResourceNotFoundException;
 import org.springdoc.api.ErrorMessage;
 import org.springframework.http.HttpStatus;
@@ -13,7 +15,24 @@ public class ControllerExceptionHandler {
     @ExceptionHandler(value = {ResourceNotFoundException.class})
     @ResponseStatus(value = HttpStatus.NOT_FOUND)
     public ErrorMessage resourceNotFoundException(ResourceNotFoundException ex, WebRequest request) {
+        return new ErrorMessage(ex.getMessage());
+    }
 
+    @ExceptionHandler(value = {AlreadyExistException.class})
+    @ResponseStatus(value = HttpStatus.BAD_REQUEST, reason = "уже зарегестрирован")
+    public ErrorMessage chatAlreadyRegisteredException(AlreadyExistException ex, WebRequest request) {
+        return new ErrorMessage(ex.getMessage());
+    }
+
+    @ExceptionHandler(value = {NotExistException.class})
+    @ResponseStatus(value = HttpStatus.BAD_REQUEST, reason = "не существует")
+    public ErrorMessage chatAlreadyRegisteredException(NotExistException ex, WebRequest request) {
+        return new ErrorMessage(ex.getMessage());
+    }
+
+    @ExceptionHandler(value = {IllegalArgumentException.class})
+    @ResponseStatus(value = HttpStatus.BAD_REQUEST, reason = "невалидные аргументы")
+    public ErrorMessage illegalArgumentException(IllegalArgumentException ex, WebRequest request) {
         return new ErrorMessage(ex.getMessage());
     }
 }
