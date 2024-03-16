9071a41c7ac29199270b1f44a5ff9e7499f54a92
NastyaLush <yfcn.if1203@gmail.com>
2024-03-16 16:31:17 +0300

    first steps

---
+++ scrapper-jooq/src/test/java/com/example/scrapperjooq/ScrapperJooqApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.scrapperjooq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScrapperJooqApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
